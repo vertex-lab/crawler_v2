68362c0a6b99d0d55bd52486f67d4ff1ec815ae7
pippellia-btc <pippellia@gmail.com>
2025-06-17 12:28:12 +0200

    added startup and shutdown separators

--- cmd/crawler/main.go
+++ cmd/crawler/main.go
@@ -30,6 +30,9 @@ func main() {
 	defer cancel()
 	go pipe.HandleSignals(cancel)
 
+	log.Printf("---------starting up the crawler --------")
+	defer log.Printf("-----------------------------------------")
+
 	config, err := config.Load()
 	if err != nil {
 		panic(err)
