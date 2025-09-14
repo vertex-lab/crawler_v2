25ba6afeb6e567b61e29f6c22d5bf34458f42672
pippellia-btc <pippellia@gmail.com>
2025-09-14 18:48:14 +0200

    moved around stuff

--- pkg/pipe/pipe_test.go
+++ pkg/pipe/pipe_test.go
@@ -17,6 +17,21 @@ var (
 	pip   string = "f683e87035f7ad4f44e0b98cfbd9537e16455a92cd38cefc4cb31db7557f5ef2"
 )
 
+type mockChecker struct {
+	pubkey string
+}
+
+func (c mockChecker) Exists(ctx context.Context, pubkey string) (bool, error) {
+	return pubkey == c.pubkey, nil
+}
+
+func print(e *nostr.Event) error {
+	fmt.Printf("\nevent ID: %v", e.ID)
+	fmt.Printf("\nevent pubkey: %v", e.PubKey)
+	fmt.Printf("\nevent kind: %d\n", e.Kind)
+	return nil
+}
+
 // Manually change pip's follow list and see if the events gets printed. Works only with `go test`
 func TestFirehose(t *testing.T) {
 	ctx, cancel := context.WithTimeout(ctx, time.Second*20)
@@ -40,19 +55,4 @@ func TestFetch(t *testing.T) {
 	if len(events) != expected {
 		t.Fatalf("expected %d events, got %d", expected, len(events))
 	}
-}
-
-type mockChecker struct {
-	pubkey string
-}
-
-func (c mockChecker) Exists(ctx context.Context, pubkey string) (bool, error) {
-	return pubkey == c.pubkey, nil
-}
-
-func print(e *nostr.Event) error {
-	fmt.Printf("\nevent ID: %v", e.ID)
-	fmt.Printf("\nevent pubkey: %v", e.PubKey)
-	fmt.Printf("\nevent kind: %d\n", e.Kind)
-	return nil
 }
