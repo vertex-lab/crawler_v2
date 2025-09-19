1de9a84ba37cf56d5b4b95781a41440a90cb7df5
pippellia-btc <pippellia@gmail.com>
2025-09-19 15:56:07 +0200

    moved buffer closer to its usage in Firehose

--- pkg/pipe/firehose.go
+++ pkg/pipe/firehose.go
@@ -203,3 +203,25 @@ func Firehose(
 		}
 	}
 }
+
+type buffer struct {
+	IDs      []string
+	capacity int
+	write    int
+}
+
+func newBuffer(capacity int) *buffer {
+	return &buffer{
+		IDs:      make([]string, capacity),
+		capacity: capacity,
+	}
+}
+
+func (b *buffer) Add(ID string) {
+	b.IDs[b.write] = ID
+	b.write = (b.write + 1) % b.capacity
+}
+
+func (b *buffer) Contains(ID string) bool {
+	return slices.Contains(b.IDs, ID)
+}
